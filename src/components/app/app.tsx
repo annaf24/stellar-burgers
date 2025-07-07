43026f228276f098ebee35c0a1b29632b6cd2bf9
Anna Filippova <afilippova240@gmail.com>
2025-07-07 19:55:24 +0300

    первоначальная настройка роутинга, добавлены ссылки в шапке

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,14 +1,73 @@
-import { ConstructorPage } from '@pages';
+import { Routes, Route, useLocation } from 'react-router-dom'
+import { ConstructorPage, Feed, ForgotPassword, Login, NotFound404, Profile, ProfileOrders, Register, ResetPassword } from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
 
-import { AppHeader } from '@components';
+import { AppHeader, IngredientDetails, Modal, OrderInfo } from '@components';
 
-const App = () => (
-  <div className={styles.app}>
-    <AppHeader />
-    <ConstructorPage />
-  </div>
-);
+const App = () => {
+
+  const location = useLocation();
+  const backgroundLocation = location.state?.background;
+  const onClose = () => window.history.back()
+
+  return (
+    <div className={styles.app}>
+      <AppHeader />
+        <Routes location={backgroundLocation || location}>
+          <Route path='/' element={<ConstructorPage />} />
+          <Route path='/feed' element={<Feed />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+          <Route path='/reset-password' element={<ResetPassword />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/profile/orders' element={<ProfileOrders />} />
+          <Route path='*' element={<NotFound404 />} />
+          <Route path='/feed/:number' element={<OrderInfo />} />
+          <Route path='/ingredients/:id' element={<IngredientDetails />} />
+          <Route path='/profile/orders/:number' element={<OrderInfo />} />
+        </Routes>
+
+        {backgroundLocation && (
+        <Routes>
+          <Route
+            path='/ingredients/:id'
+            element={
+              <Modal
+                title='Детали ингридиента'
+                onClose={onClose}
+              >
+                <IngredientDetails />
+              </Modal>
+            }
+          />
+          {/* <Route
+            path='/feed/:number'
+            element={
+              <Modal
+                title=''
+                onClose={onClose}
+              >
+                <OrderInfo />
+              </Modal>
+            }
+          />
+          <Route
+            path='/profile/orders/:number'
+            element={
+              <Modal
+                title=''
+                onClose={onClose}
+              >
+                <OrderInfo />
+              </Modal>
+            }
+          /> */}
+        </Routes>
+      )}
+    </div>
+  );
+};
 
 export default App;
